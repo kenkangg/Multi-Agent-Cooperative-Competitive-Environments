5cce810642f6705cbd2a6d707435e1243cc66a50
Kenny Kang <kennykang@berkeley.edu>
2018-07-23 12:22:17 -0700

    Added updated, training model. Not optimal, but showing improvement.

--- src/run.py
+++ src/run.py
@@ -4,34 +4,48 @@ import gym
 import os
 import sys
 import numpy as np
+import pickle
 
 from pommerman.agents import SimpleAgent, RandomAgent, PlayerAgent, BaseAgent
-from pommerman.configs import ffa_v1_env
+from pommerman.configs import ffa_v0_fast_env, ffa_competition_env
 from pommerman.envs.v0 import Pomme
 from pommerman.characters import Bomber
 from pommerman import utility
 
 from tensorforce.agents import PPOAgent
+from tensorforce.agents import DQNAgent
 from tensorforce.execution import Runner
 from tensorforce.contrib.openai_gym import OpenAIGym
+from tensorforce.agents import Agent
+
+from pommerman import utility
+
+import argparse
+
+import tensorflow as tf
 
 import numpy as np
 
-"""
-State Space: Alive [<=4]
-             Board {0: open space,
-                    1: unbreakable,
-                    2: breakable}
-             Bomb Blast strength
-             Bomb_life
-             Position
-             Blast_strength
-             Can_Kick,
-             Teammate
-             Ammo
-             Enemies
+import json
 
-"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-test', action='store_true')
+parser.add_argument('-resume', action='store_true')
+parser.add_argument('-exp', type=str)
+args = parser.parse_args()
+
+print(args.test)
+
+EXPERIMENT_NAME = args.exp
+BATCH_SIZE = 32
+VISUALIZE = False
+EPISODES = 100000
+# EXPERIMENT_NAME = 'model_timestep_reward'
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+sess = tf.Session(config=config)
 
 
 
@@ -78,10 +92,41 @@ class WrappedEnv(OpenAIGym):
         obs = self.gym.get_observations()
         all_actions = self.gym.act(obs)
         all_actions.insert(self.gym.training_agent, actions)
+
+
+        alive_before = self.check_alive()
+        bomb_before = self.check_bombs()
         state, reward, terminal, _ = self.gym.step(all_actions)
+
+        alive_after = self.check_alive()
+        bomb_after = self.check_bombs()
+
         agent_state = featurize(state[self.gym.training_agent])
         agent_reward = reward[self.gym.training_agent]
+        ## print(bomb_before, bomb_after)
+        if agent_reward == 1:
+            agent_reward *= 5
+        elif alive_before != alive_after and self.gym._agents[self.gym.training_agent].is_alive and alive_after != 1:
+            print(alive_before, alive_after)
+            # agent_reward += 0.25 * (alive_before - alive_after)
+            if bomb_before < bomb_after:
+                print(bomb_before, bomb_after)
+                agent_reward += 1  * (alive_before - alive_after)
+        # agent_reward -= 0.001
         return agent_state, terminal, agent_reward
+
+    def check_alive(self):
+        """ Self Added """
+        count = 0
+        for agent in self.gym._agents:
+            if agent.is_alive:
+                count += 1
+        return count
+
+    def check_bombs(self):
+        """ Self Added """
+        obs = self.gym.get_observations()
+        return utility.make_np_float([obs[0]["ammo"]])
 
     def reset(self):
         obs = self.gym.reset()
@@ -91,23 +136,30 @@ class WrappedEnv(OpenAIGym):
 def main():
     # Print all possible environments in the Pommerman registry
     # Instantiate the environment
-    config = ffa_v1_env()
+    DETERMINISTIC = False
+    VISUALIZE = False
+
+    if args.test:
+        DETERMINISTIC = True
+        VISUALIZE = True
+
+    config = ffa_competition_env()
     env = Pomme(**config["env_kwargs"])
     env.seed(0)
 
     # Create a Proximal Policy Optimization agent
-    agent = PPOAgent(
-        states=dict(type='float', shape=env.observation_space.shape),
-        actions=dict(type='int', num_actions=env.action_space.n),
-        network=[
-            dict(type='dense', size=64),
-            dict(type='dense', size=128),
-            dict(type='dense', size=64)
-        ],
-        batching_capacity=1000,
-        step_optimizer=dict(
-            type='adam',
-            learning_rate=1e-4
+    with open('ppo.json', 'r') as fp:
+            agent = json.load(fp=fp)
+
+    with open('mlp2_lstm_network.json', 'r') as fp:
+            network = json.load(fp=fp)
+
+    agent = Agent.from_spec(
+        spec=agent,
+        kwargs=dict(
+            states=dict(type='float', shape=env.observation_space.shape),
+            actions=dict(type='int', num_actions=env.action_space.n),
+            network=network
         )
     )
 
@@ -124,21 +176,67 @@ def main():
     env.set_init_game_state(None)
 
     # Instantiate and run the environment for 5 episodes.
-    wrapped_env = WrappedEnv(env)
+    if VISUALIZE:
+        wrapped_env = WrappedEnv(env, True)
+    else:
+        wrapped_env = WrappedEnv(env)
+
     runner = Runner(agent=agent, environment=wrapped_env)
 
+    rewards = []
+    episodes = []
     def episode_finished(r):
+        nonlocal episodes
+        nonlocal rewards
         print("Finished episode {ep} after {ts} timesteps (reward: {reward})".format(ep=r.episode, ts=r.episode_timestep,
-                                                                                     reward=r.episode_rewards[-1]))
+                                                                             reward=r.episode_rewards[-1]))
+        if r.episode % 1000 == 0:
+            agent.save_model(('./{}').format(EXPERIMENT_NAME), False)
+            try:
+                prev_data = pickle.load(open(EXPERIMENT_NAME, "rb"))
+                prev_len = len(prev_data[0])
+                prev_data[0].extend(rewards)
+                rewards = []
+                prev_data[1].extend(episodes)
+                episodes = []
+                pickle.dump(prev_data, open(EXPERIMENT_NAME, "wb"))
+            except (OSError, IOError) as e:
+                pickle.dump([rewards, episodes], open(EXPERIMENT_NAME, "wb"))
+        if r.episode_rewards[-1] >= 5:
+            print()
+            print()
+            print()
+            print("WINNER WINNER CHICKEN DINNER")
+        episodes.append(r.episode)
+        rewards.append(r.episode_rewards[-1])
         return True
 
-    runner.run(episodes=300, max_episode_timesteps=2000, episode_finished=episode_finished)
-    print("Stats: ", runner.episode_rewards, runner.episode_timesteps, runner.episode_times)
+    # Restore, Train, and Save Model
+    if args.test or args.resume: # If test, change settings and restore model
+        agent.restore_model('./','PPO_K_someS_500batch_biggerreward_99dis')
+    runner.run(episodes=EPISODES, max_episode_timesteps=2000, episode_finished=episode_finished, deterministic=False)
+
+    if not args.test:
+        agent.save_model(('./{}').format(EXPERIMENT_NAME), False)
+    print("Stats: ", runner.episode_rewards[-5:], runner.episode_timesteps[-5:])
+
+    #Dump reward values
+    try:
+        prev_data = pickle.load(open(EXPERIMENT_NAME, "rb"))
+        prev_len = len(prev_data[0])
+        prev_data[0].extend(rewards)
+        prev_data[1].extend(episodes)
+        print(episodes)
+        pickle.dump(prev_data, open(EXPERIMENT_NAME, "wb"))
+    except (OSError, IOError) as e:
+        pickle.dump([rewards, episodes], open(EXPERIMENT_NAME, "wb"))
 
     try:
         runner.close()
     except AttributeError as e:
         pass
+
+
 
 if __name__ == '__main__':
     main()
